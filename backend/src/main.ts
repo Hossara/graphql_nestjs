24150f3a4f95efcc4a14d7f3ce56a82107bbd13e
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-05-11 20:49:53 +0330

    Backend: Init logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import {LoggerBuilder} from "./logger"
+import env from "./config/env"
+
+export const Logger = new LoggerBuilder("Actin app")
+const log = Logger.Builder()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(8081)
+  await app.listen(env().PORT, '0.0.0.0')
 }
 bootstrap()
