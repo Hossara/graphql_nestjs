34e44036ec695e4a296cd46793f78805fc01aa1f
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-05-11 20:51:04 +0330

    Backend: Import mongodb and configuration services to nest main module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import {MongooseModule} from "@nestjs/mongoose"
+import {Logger} from "./main"
+import {Module} from "@nestjs/common"
+import env from "./config/env"
+import {ConfigModule} from "@nestjs/config"
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Global env config
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [env]
+    }),
+
+    // Mongoose config
+    MongooseModule.forRootAsync({
+      useFactory: () => {
+        Logger.Builder().startup_info("MongoDB", `MongoDB is running at ${env().MONGO_CONNECTION}`)
+
+        return {
+          uri: env().MONGO_CONNECTION
+        }
+      }
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
