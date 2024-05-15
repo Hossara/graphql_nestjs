9497914a2270c61d9419e7597ffd6269b773e973
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-05-15 19:42:58 +0330

    Backend: Add two queries to tasks GraphQL resolver (get all, insert)

--- backend/src/modules/tasks/tasks.resolver.ts
+++ backend/src/modules/tasks/tasks.resolver.ts
@@ -1,6 +1,21 @@
-import {Resolver} from "@nestjs/graphql"
+import {Args, Mutation, Query, Resolver} from "@nestjs/graphql"
+import {Task, taskInputToSchema} from "../../schema/tasks.schema"
+import {TasksService} from "./tasks.service"
+import {TaskInput} from "./tasks.input"
 
-@Resolver()
+@Resolver(() => Task)
 export class TasksResolver {
+    constructor(private readonly tasksService: TasksService) {}
 
+    @Query(() => [Task])
+    tasks(): Promise<Task[]> {
+        return this.tasksService.findAllTasks()
+    }
+
+    @Mutation(() => Task)
+    async addRecipe(
+        @Args('new_task') new_task: TaskInput,
+    ) {
+        return await this.tasksService.insert(taskInputToSchema(new_task))
+    }
 }
