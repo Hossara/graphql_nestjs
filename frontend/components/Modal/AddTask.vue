9169b38fbdc13ace77045b5f9d757229c87f56dc
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-05-18 14:54:09 +0330

    Frontend: Add task submit method and form validation

--- frontend/components/Modal/AddTask.vue
+++ frontend/components/Modal/AddTask.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { z } from 'zod'
 import {format} from "date-fns"
 
 const props = defineProps<{ is_open: boolean }>()
@@ -6,8 +7,38 @@ defineEmits(["close"])
 
 const modalIsOpen = computed(() => props.is_open)
 
-const date = shallowRef(null)
-const priority = shallowRef(0)
+const state = reactive({
+  name: undefined,
+  description: undefined,
+  date: null,
+  priority: 0
+})
+
+const schema = z.object({
+  name: z.string()
+      .min(3, "Task name must be at least 3 characters")
+      .max(100, "Task name must be at least 100 characters"),
+  description: z.string()
+      .min(3, "Task description must be at least 3 characters")
+      .max(500, "Task description must be at least 100 characters")
+})
+
+const submit = async () => {
+
+
+  await GqlAddTask({
+    task: {
+      "name": "first task",
+      "created_date": new Date(),
+      "description": "test",
+      "priority": 1
+    }
+  }).then((value) => {
+    console.log(value.addTask)
+  }).catch(reason => {
+    console.log(reason)
+  })
+}
 </script>
 
 <template>
@@ -21,34 +52,41 @@ const priority = shallowRef(0)
             name="i-heroicons-x-mark-solid" @click="$emit('close')"/>
       </h2>
 
-      <form class="flex flex-col gap-8 h-full">
-        <UFormGroup label="Name" size="xl" required>
-          <UInput color="white" size="lg" class="!text-xl mt-2" variant="outline" placeholder="Morning exercise" />
+      <UForm :state="state" :schema="schema" class="flex flex-col gap-8 h-full" @submit="submit">
+        <UFormGroup name="name" label="Name" size="xl" required eager-validation>
+          <UInput
+              v-model="state.name" color="white" size="lg"
+              class="!text-xl mt-2" variant="outline"
+              placeholder="Morning exercise" />
         </UFormGroup>
 
-        <UFormGroup label="Description" size="xl" required>
-          <UTextarea color="white" size="lg" :rows="8" class="!text-xl mt-2" variant="outline" placeholder="Morning exercise" />
+        <UFormGroup name="description" label="Description" size="xl" required eager-validation>
+          <UTextarea
+              v-model="state.description"
+              color="white" size="lg" :rows="8"
+              class="!text-xl mt-2" variant="outline"
+              placeholder="Morning exercise" />
         </UFormGroup>
 
         <UFormGroup label="Expiration date" size="xl">
-          <VDatePicker is-dark v-model="date">
+          <VDatePicker is-dark v-model="state.date">
             <template #default="{ togglePopover }">
               <UButton
                   icon="i-heroicons-calendar-days-20-solid"
                   size="lg" variant="soft" block class="text-lg"
                   @click="togglePopover">{{
-                  date ? format(date, 'd MMM, yyy') : 'Select date'
+                  state.date ? format(state.date, 'd MMM, yyy') : 'Select date'
                 }}</UButton>
             </template>
           </VDatePicker>
         </UFormGroup>
 
-        <UFormGroup :label="`Priority: ${priority}`" size="xl">
-          <URange class="mt-4" :step="1" v-model:model-value="priority" size="md" :min="1" :max="3" />
+        <UFormGroup :label="`Priority: ${state.priority}`" size="xl">
+          <URange class="mt-4" :step="1" v-model:model-value="state.priority" size="md" :min="1" :max="3" />
         </UFormGroup>
 
         <UButton type="submit" block size="xl" class="text-lg mt-auto" color="green">Add Task</UButton>
-      </form>
+      </UForm>
     </div>
   </USlideover>
 </template>
